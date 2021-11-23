c155875c478013157eeacd803b36ea55433d9593
ThiagoLR <thiaguinhoLrodrigues007@gmail.com>
2021-11-23 17:03:55 -0300

    arrumado

--- view/formAlterarUsuario.php
+++ view/formAlterarUsuario.php
@@ -12,19 +12,19 @@
 <form id="formusu" method="POST" action="../controller/alterarUsuario.php">
 
 <div class="mb-3">
-    <label for="codigo" class="form-label">Código</label>
-    <input type="text" class="form-control" name="codUsu" readonly value="<?= $usuario["idusu"]?>" id="Codigo" aria-describedby="emailHelp">
+    <label class="subtitles" for="codigo" class="form-label">Código</label>
+    <input type="text" class="form-control" name="idUsu" readonly value="<?= $usuario["idusu"]?>" id="Codigo" aria-describedby="emailHelp">
   </div>
   <div class="mb-3">
-    <label for="Email" class="form-label">Email</label>
-    <input type="email" required class="form-control" name="codUsu" value="<?= $usuario["emailusu"] ?>" id="Email" aria-describedby="emailHelp">
+    <label class="subtitles" for="Email" class="form-label">Email</label>
+    <input type="email" required class="form-control" name="email" value="<?= $usuario["emailusu"] ?>" id="Email" aria-describedby="emailHelp">
   </div>
 
   <div class="mb-3">
-    <label for="senha" class="form-label">Senha</label>
-    <input type="password" required name="codUsu" class="form-control" id="senha">
+    <label class="subtitles" for="senha" class="form-label">Senha</label>
+    <input type="password" required name="senha" class="form-control" id="senha">
   </div>
-  <button type="submit" class="btn btn-primary">Cadastrar</button>
+  <button type="submit" class="btn btn-primary">Alterar</button>
 </form>
 
 <?php
